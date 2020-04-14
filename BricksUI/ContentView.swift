ce365675654f1824aa2468d5d166c48a2ca65112
Fabio Staiano <fabio.staiano@gmail.com>
2020-04-14 16:02:41 +0200

    Initial Commit

---
+++ BricksUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BricksUI
+//
+//  Created by Fabio Staiano on 14/04/2020.
+//  Copyright © 2020 Fabio Staiano. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
