a9a9c0070a716b0dc59e7b8944e85cf55de9227e
Samuel Kebis <samokebis@gmail.com>
2020-05-07 12:24:31 +0200

    typography - usable on any view as modifier

--- BricksUI/Typography.swift
+++ BricksUI/Typography.swift
@@ -6,86 +6,91 @@
 //  Copyright © 2020 Fabio Staiano. All rights reserved.
 //
 
-import Foundation
 import SwiftUI
 
-extension Text {
-
+struct BSTypography: ViewModifier {
+    
     enum Style {
-        case h1, h2, h3, h4, h5, h6, // Titles
-             s1, s2, // Subtitles
-             p1, p2, // Paragraphs
-             c1, c2  // Captions
+        
+        /// Titles
+        case h1, h2, h3, h4, h5, h6
+        
+        /// Subtitles
+        case s1, s2
+        
+        /// Paragraphs
+        case p1, p2
+        
+        /// Captions
+        case c1, c2
     }
-
-    func bsTypo(_ style: Style, _ color: Color = .bsFontStd) -> Text {
+    
+    var style: Style
+    
+    func body(content: Content) -> some View {
         switch style {
-        case .h1:
-            return
-                foregroundColor(color)
-            .font(.system(size: 36))
-            .fontWeight(.bold)
-        case .h2:
-            return
-                foregroundColor(color)
-            .font(.system(size: 32))
-            .fontWeight(.bold)
-        case .h3:
-            return
-                foregroundColor(color)
-            .font(.system(size: 30))
-            .fontWeight(.bold)
-        case .h4:
-            return
-                foregroundColor(color)
-            .font(.system(size: 26))
-            .fontWeight(.bold)
-        case .h5:
-            return
-                foregroundColor(color)
-            .font(.system(size: 22))
-            .fontWeight(.bold)
-        case .h6:
-            return
-                foregroundColor(color)
-            .font(.system(size: 18))
-            .fontWeight(.bold)
-        case .s1:
-            return
-                foregroundColor(color)
-            .font(.system(size: 15))
-            .fontWeight(.semibold)
-        case .s2:
-            return
-                foregroundColor(color)
-            .font(.system(size: 13))
-            .fontWeight(.semibold)
-        case .p1:
-            return
-                foregroundColor(color)
-            .font(.system(size: 15))
-            .fontWeight(.regular)
-        case .p2:
-            return
-                foregroundColor(color)
-            .font(.system(size: 13))
-            .fontWeight(.regular)
-        case .c1:
-            return
-                foregroundColor(color)
-            .font(.system(size: 12))
-            .fontWeight(.regular)
-        case .c2:
-            return
-                foregroundColor(color)
-            .font(.system(size: 12))
-            .fontWeight(.bold)
+        case .h1: return content
+            .font(.system(size: 36, weight: .bold))
+        case .h2: return content
+            .font(.system(size: 32, weight: .bold))
+        case .h3: return content
+            .font(.system(size: 30, weight: .bold))
+        case .h4: return content
+            .font(.system(size: 26, weight: .bold))
+        case .h5: return content
+            .font(.system(size: 22, weight: .bold))
+        case .h6: return content
+            .font(.system(size: 18, weight: .bold))
+        case .s1: return content
+            .font(.system(size: 15, weight: .semibold))
+        case .s2: return content
+            .font(.system(size: 13, weight: .semibold))
+        case .p1: return content
+            .font(.system(size: 15, weight: .regular))
+        case .p2: return content
+            .font(.system(size: 13, weight: .regular))
+        case .c1: return content
+            .font(.system(size: 12, weight: .regular))
+        case .c2: return content
+            .font(.system(size: 12, weight: .bold))
         }
     }
 }
 
+extension View {
+    func bsTypo(_ style: BSTypography.Style, color: Color = .bsFontStd) -> some View {
+        self
+            .modifier(BSTypography(style: style))
+            .foregroundColor(color)
+    }
+}
+
+
 struct Typography_Previews: PreviewProvider {
     static var previews: some View {
-        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/.bsTypo(.h1)
+        VStack(spacing: 10) {
+            Group {
+                Text("Typography h1").bsTypo(.h1, color: .blue)
+                Text("Typography h1").bsTypo(.h1, color: .green)
+                Text("Typography h1").bsTypo(.h1, color: .pink)
+            
+                Text("Typography h1").bsTypo(.h1)
+                Text("Typography h2").bsTypo(.h2)
+                Text("Typography h3").bsTypo(.h3)
+                Text("Typography h4").bsTypo(.h4)
+                Text("Typography h5").bsTypo(.h5)
+                Text("Typography h6").bsTypo(.h6)
+            }
+            Group {
+                Text("Typography h1").bsTypo(.s1)
+                Text("Typography h2").bsTypo(.s2)
+                
+                Text("Typography p1").bsTypo(.p1)
+                Text("Typography p2").bsTypo(.p2)
+                
+                Text("Typography c1").bsTypo(.c1)
+                Text("Typography c2").bsTypo(.c2)
+            }
+        }
     }
 }
